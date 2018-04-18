4b92b6de66bc1a244c3e79b2758aed668f42188d
dbaldwin77 <daveandjaneen@gmail.com>
2018-04-18 17:16:49 -0500

    added editing to delete row

--- toDo/TableViewController.swift
+++ toDo/TableViewController.swift
@@ -15,6 +15,8 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem
+        navigationItem.leftBarButtonItem?.tintColor = UIColor.white
         for i in 1...20 {
           
             listItem.append(DataItem(toDoitem: "To Do list item #\(i)"))
@@ -24,11 +26,10 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-   
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return listItem.count
-        }
+        return listItem.count
+    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
             return 1
@@ -42,7 +43,23 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
             cell.detailTextLabel?.text = item.toDoitem
             return cell
         }
-
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            listItem.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+        if editing {
+            tabel_View.setEditing(true, animated: true)
+        } else {
+            tabel_View.setEditing(false, animated: true)
+        }
+    }
 
 }
 
